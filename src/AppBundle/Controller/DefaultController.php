c48300f8dd5b4cc2c4cd4c876d718ecc15f94053
Vlad <hraban@vivec.co.uk>
2016-03-07 14:17:05 +0000

    Delete hello world route, action and template

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,12 +6,5 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function helloAction($name)
-    {
-        return $this->render('AppBundle:Default:hello.html.twig',
-            [
-                "name" => $name
-            ]
-        );
-    }
+
 }
