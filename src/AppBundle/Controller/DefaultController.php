11d986b97419e900978f7cbbfc59ae572f895fd5
Vlad <hraban@vivec.co.uk>
2016-03-07 14:26:06 +0000

    Add a placeholder index page

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,5 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-
+    public function indexAction()
+    {
+        return $this->render('AppBundle:Default:index.html.twig');
+    }
 }
