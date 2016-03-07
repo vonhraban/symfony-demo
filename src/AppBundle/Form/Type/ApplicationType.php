b23ab3fc7840185360bcd5ec089a3be78e1ad58a
Vlad <hraban@vivec.co.uk>
2016-03-07 15:35:38 +0000

    Define application form type as service, pass client and retrieve list of countries for dropdown

--- src/AppBundle/Form/Type/ApplicationType.php
+++ src/AppBundle/Form/Type/ApplicationType.php
@@ -1,6 +1,7 @@
 <?php
 namespace AppBundle\Form\Type;
 
+use AppBundle\DependencyInjection\RestCountriesClient;
 use AppBundle\Entity\Application;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -11,6 +12,21 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ApplicationType extends AbstractType
 {
+    /**
+     * @var RestCountriesClient a client to talk to RestCountries API
+     */
+    protected $restCountriesClient;
+
+    /**
+     * A constructor, sets up dependencies
+     *
+     * @param RestCountriesClient $restCountriesClient
+     */
+    public function __construct(RestCountriesClient $restCountriesClient)
+    {
+        $this->restCountriesClient = $restCountriesClient;
+    }
+
     /**
      * {@inheritdoc}
      */
@@ -23,7 +39,10 @@ class ApplicationType extends AbstractType
                     ]
                 )
                 ->add('age', IntegerType::class)
-                ->add('country', TextType::class)
+                ->add('country', ChoiceType::class, [
+                        "choices" => $this->restCountriesClient->getEuropeanCountries(),
+                    ]
+                )
                 ->add('save', SubmitType::class);
     }
 }
